a5db67a031d8c71918a5123a6f1fc8a013b74094
Mario Masciovecchio <mario.masciovecchio@cern.ch>
2018-03-08 07:18:31 -0800

    Cleaning up

--- HistoAnalyzer/python/purge.sh
+++
@@ -1,7 +0,0 @@
-crab purge -d crab_projects/crab_stoppedTracks_Data
-crab purge -d crab_projects/crab_stoppedTracks_Data2016_runH
-crab purge -d crab_projects/crab_stoppedTracks_DataV2
-crab purge -d crab_projects/crab_stoppedTracks_DataV3
-crab purge -d crab_projects/crab_stoppedTracks_MC
-crab purge -d crab_projects/crab_stoppedTracks_MCV2
-crab purge -d crab_projects/crab_stoppedTracks_MCV3
