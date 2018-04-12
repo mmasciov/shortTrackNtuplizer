89ba8c4bc5e551561c36c104367f8633e39c29b5
dpg <dgilbert@physics.ucsd.edu>
2018-04-12 03:34:22 -0700

    tree*.py comments

--- treefriend.py
+++ treefriend.py
@@ -5,14 +5,23 @@ if len(sys.argv)!=6:
     print "Example: python treefriend.py mt2baby.root mt2 stbaby.root myTree mt2stbaby.root"
     exit(1)
 
+# first file name
 ffn = sys.argv[1]
+# first tree name
 ftn = sys.argv[2]
+# second file name
 sfn = sys.argv[3]
+# second tree name
 stn = sys.argv[4]
+# output file name
 ofn = sys.argv[5]
+# output mt2 tree name
 otn = "mt2st"
+# output mt2 tree title
 ott = "A mt2 baby-tree edition"
+# output short track tree name
 otstn = "st"
+# output short track tree title
 otstt = "A short-track baby-tree edition"
 
 print ffn
@@ -21,18 +30,24 @@ print sfn
 print stn
 print ofn
 
+# First file
 ff = ROOT.TFile(ffn, "READ")
+# First tree
 ft = ff.Get(ftn).Clone()
 
+# Number of entries in the first file
 Nf = ft.GetEntries()
 
 print "Nf = " + str(Nf)
 
+# Second file
 sf = ROOT.TFile(sfn, "READ")
+# Second tree
 st = sf.Get(stn).Clone()
 
 Ns = st.GetEntries()
 
+# first tree run, lumi, evt, index
 fr=[]
 fl=[]
 fe=[]
@@ -48,6 +63,7 @@ for index in range(0,Nf):
 
 fzip = zip(fr,fl,fe,fi)
 
+# second tree run, lumi, evt, index
 sr=[]
 sl=[]
 se=[]
@@ -63,24 +79,29 @@ for index in range(0,Ns):
 
 szip = zip(sr,sl,se,si)
 
+# Indices of matched entries in each tree. fii[i] points to the same event as sii[i], but fii[i] is mot necessarily the same *number* as sii[i],
+# as they may be at different positions within their respective sorted trees.
 fii = []
 sii = []
 
 print "Number of mt2 events: " + str(len(fzip))
 print "Number of st events: " + str(len(szip))
 
+# Keep track of last location we found a match. Since trees are sorted, we'll never find a match for the next entry in the first tree 
+# at a location before the last match in the second tree. 
 lastY = 0
 
 for x in xrange(len(fzip)):
     if (x % 10000 == 0): print "Scanning through y values for x = " + str(x) + " starting from y = " + str(lastY)
     for y in xrange(lastY,len(szip)):
         if fzip[x][0]==szip[y][0] and fzip[x][1]==szip[y][1] and fzip[x][2]==szip[y][2]:
+            # Found a match, save the respective indices
             fii.append(fzip[x][3])
             sii.append(szip[y][3])
             lastY=y
             if (len(fii) % 10000 == 0): print "total matches = " + str(len(fii))
             break
-        else:
+        else: # The first three mean there can be no match for our first event; we've overshot. Skip if so. Otherwise, continue.
             if fzip[x][0]<szip[y][0]:
                 break
             elif fzip[x][0]==szip[y][0] and fzip[x][1]<szip[y][1]:
@@ -98,12 +119,9 @@ if len(fii) != len(sii):
     exit(1)
 
 for x in xrange(len(fii)):
-    if (x % 10000 == 0): print "Marking " + str(x) + "th event to be saved"
+    if ( (x+1) % 10000 == 0): print "Marking " + str(x+1) + "th event to be saved"
     felist.Enter(fii[x])
     selist.Enter(sii[x])
-    if (x >= 41060000):
-        print str(x) + "th mt2 event: " + str(fii[x])
-        print str(x) + "th st event: " + str(sii[x])
 
 print "Saving events"
 
@@ -112,6 +130,7 @@ st.SetEventList(selist)
 
 print "Creating output trees"
 
+# Copy the filtered mt2 tree ("first tree") into the output mt2 tree
 of = ROOT.TFile(ofn, "RECREATE")
 ot = ft.CopyTree("")
 
@@ -120,6 +139,7 @@ ff.Close()
 
 print "First output tree finished"
 
+# Copy the filtered st tree ("second tree") into the output st tree
 st.SetBranchStatus("run", False)
 st.SetBranchStatus("lumi", False)
 st.SetBranchStatus("evt", False)
--- treesort.py
+++ treesort.py
@@ -10,9 +10,13 @@ finname  = sys.argv[1]
 tinname  = sys.argv[2]
 foutname = sys.argv[3]
 
+# input file
 fin = ROOT.TFile(finname)
+# input tree
 tin = fin.Get(tinname)
+# output file
 fout = ROOT.TFile(foutname,"recreate")
+# output tree
 tout = tin.CloneTree(0)
 
 N = tin.GetEntries()
@@ -28,27 +32,35 @@ for index in range(0,N):
     l.append(tin.lumi)
     e.append(tin.evt)
     i.append(index)
-    
+
+# zipped arrays    
 za = zip(r,l,e,i)
+# zipped arrays sorted
 zas = sorted(za, key = lambda x:(x[0],x[1],x[2]))
+# sorted indices
 ii = [zas[x][3] for x in range(0,len(zas))]
 
 next = ROOT.TIter(tin.GetListOfBranches())
 bin = ROOT.TBranch()
 bout = ROOT.TBranch()
 
+# Loop over all branches
 bin = next()
 while bin:
     if not bin:
         break
+    # Get branch with same name as branch to-be-copied
     bout = tout.GetBranch(bin.GetName())
     print "processing branch ", bin.GetName()
+    # LoadBaskets forces the entire input branch into memory. Necessary for random access.
     bin.LoadBaskets()
     for index in range(0,N):
+        # Place branch entry that was sorted into position index into the index-th entry of the output tree
         bin.GetEntry(ii[index])
         bout.Fill()
     bin.DropBaskets()
     tout.AutoSave()
+    # Perhaps an explicit bout.DropBaskets() could be useful here?
     bin = next()
 tout.SetEntries(N)
 tout.Print()
